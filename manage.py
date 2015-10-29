33f2a6e437c6df98cd1e13fc7ecf79e641fae5db
jiho <polaris341@gmail.com>
2015-10-29 09:46:14 +0900

    move config.py to application directory

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
-from app import create_app, db
+from application import create_app, db
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
